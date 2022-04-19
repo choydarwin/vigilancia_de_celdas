4a0f31da55251f0618e4252b983205faa0047db4
darwin <choydarwin@gmail.com>
2022-04-19 14:06:04 -0600

    Interfaz simple, con reloj en terminal

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from kivy.app import App 
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -6,12 +7,18 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.properties import StringProperty
 import time
 
 
 class FirstWindow(Screen):
     pass
-
+    
+class ClockText(Label):
+    def update(self, *args):
+        self.text = time.strftime('%I'+':'+'%M' + ':' + '%S' +  ' %p')
+        print(self.text)
+        return ClockText
 
 class SecondWindow(Screen):
     pass
@@ -26,6 +33,8 @@ kv = Builder.load_file("my.kv")
 
 class FirstApp(App):
     def build(self):
+        clocktext = ClockText()
+        Clock.schedule_interval(clocktext.update, 1)
         return kv
 
 if __name__ == '__main__':
