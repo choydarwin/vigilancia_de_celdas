10bb4771f2293bc08c311973ac7836d0b7983590
darwin <choydarwin@gmail.com>
2022-04-21 11:58:34 -0600

    Se agrego spinners de configuracion

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager, RiseInTransition
 from kivy.lang import Builder
+from kivy.uix.spinner import Spinner
 import time
 
 Builder.load_file('main.kv')
@@ -11,8 +12,16 @@ Builder.load_file('main.kv')
 class Principal(Screen):
     pass
 
-class Otra(Screen):
-    pass
+class Configuracion(Screen):
+    
+    def spinner_frecuencia_de_vigilancia(self, value):
+        print("Se selecciono hacer la vigilancia: " + value)
+    
+    def spinner_dia(self, value):
+        print("El dia de la vigilancia sera:  " + value)
+
+    def spinner_hora_de_inicio(self, value):
+        print("La hora de vigilancia sera: " + value)
 
 class ClockLabel(Label):
     def __init__(self, **kwargs):
@@ -22,8 +31,8 @@ class ClockLabel(Label):
         self.text = time.strftime('%I:%M:%S')
 
 screen_manager = ScreenManager(transition = RiseInTransition())
-screen_manager.add_widget(Principal(name ="principal"))
-screen_manager.add_widget(Otra(name ="otra"))
+screen_manager.add_widget(Principal(name ="main"))
+screen_manager.add_widget(Configuracion(name ="config"))
  
 class VigilanciaApp(App):
     def build(self):
