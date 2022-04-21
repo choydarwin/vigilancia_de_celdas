13097c898dfe50ec97cf5b6a0b2369c3a3ac1524
darwin <choydarwin@gmail.com>
2022-04-21 10:39:06 -0600

    Interfaz con dos Screen, principal y configuracion con reloj en pantalla ---- NUEVO COMIENZO :D

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
+from turtle import Screen
 from kivy.app import App
 from kivy.config import Config
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager, RiseInTransition
 from kivy.lang import Builder
 from kivy.core.window import Window
 import time
 
-Window.size = (400, 400)
+
 Builder.load_string("""
-<Layout>
-    Label:
-        text:"Vigilancia de celdas patron"
-    ClockLabel:
-        id: clock_label
-        size_hint: 0.75,1
+<Principal>
+    GridLayout:
+        cols:1
+        size: root.width, root.height
+
+        Label:
+            text:"Vigilancia de celdas patron"
+        ClockLabel:
+            id: clock_label
+            size_hint: 0.75,1
+        Button:
+            text: "Configuracion"
+            background_color : 0, 0, 1, 1
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.transition.direction = 'left'
+                root.manager.transition.duration = 1
+                root.manager.current = 'otra'
+
+
+<Otra>
+    GridLayout:
+        cols:1
+        size: root.width, root.height
+        Label:
+            text:"Settings"
+        ClockLabel:
+            id: clock_label
+            size_hint: 0.75,1
+        Button:
+            text: "Regresar"
+            background_color : 0, 0, 1, 1
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.transition.direction = 'left'
+                root.manager.transition.duration = 1
+                root.manager.current = 'principal'
+
+
 """)
 
-class Layout(BoxLayout):
+
+class Principal(Screen):
+    pass
+
+class Otra(Screen):
     pass
 
 class ClockLabel(Label):
@@ -26,11 +67,15 @@ class ClockLabel(Label):
         Clock.schedule_interval(self.update, 1)
     def update(self, *args):
         self.text = time.strftime('%I:%M:%S')
- 
-class DigitalClockApp(App):
-    def build(self):
-        return Layout()
 
-clock = DigitalClockApp()
+screen_manager = ScreenManager(transition = RiseInTransition())
+screen_manager.add_widget(Principal(name ="principal"))
+screen_manager.add_widget(Otra(name ="otra"))
+ 
+class VigilanciaApp(App):
+    def build(self):
+        return screen_manager
+
+clock = VigilanciaApp()
 
 clock.run()
