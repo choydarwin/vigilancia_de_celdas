05dd83e5b33dd126868f090d7af067f2e1fbd479
darwin <choydarwin@gmail.com>
2022-04-22 12:22:54 -0600

    Spinners para seleccionar hora, minuto, dia, mes... de inicio --- TODO LO DEMAS OK

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from ast import Pass
+from string import octdigits
 from turtle import Screen
 from kivy.app import App
 from kivy.clock import Clock
@@ -7,12 +9,17 @@ from kivy.lang import Builder
 from kivy.uix.spinner import Spinner
 import time
 
+hora=""
+
 Builder.load_file('main.kv')
 
 class Principal(Screen):
     pass
 
 class Configuracion(Screen):
+
+    minutos = [str(i) for i in range (60)]
+    horas = [str(i) for i in range(24)]
     
     def spinner_frecuencia_de_vigilancia(self, value):
         print("Se selecciono hacer la vigilancia: " + value)
@@ -23,12 +30,25 @@ class Configuracion(Screen):
     def spinner_hora_de_inicio(self, value):
         print("La hora de vigilancia sera: " + value)
 
+    def spinner_minuto_de_inicio(self, value):
+        print("El minuto inicio sera: " + value)
+
 class ClockLabel(Label):
     def __init__(self, **kwargs):
         super(ClockLabel, self).__init__(**kwargs)
         Clock.schedule_interval(self.update, 1)
     def update(self, *args):
-        self.text = time.strftime('%I:%M:%S')
+
+        global dia, hora, mes, minuto, segundo, año
+        hora=time.strftime('%H')
+        minuto=time.strftime('%M')
+        segundo=time.strftime('%S')
+        dia = time.strftime('%A')
+        mes = time.strftime('%B')
+        año = time.strftime('%g')
+
+        print(año + ": " + mes + ": " + dia + ": " + hora + ": " +  minuto + ": " + segundo)
+        self.text = time.strftime('%I:%M:%S') 
 
 screen_manager = ScreenManager(transition = RiseInTransition())
 screen_manager.add_widget(Principal(name ="main"))
